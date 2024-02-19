2ead779e395f96f76586d2e5fade0c2de147e7c4
Ánh Nguyễn <118251984+Anhnguyen12062003@users.noreply.github.com>
2024-02-19 17:47:23 +0700

    UPDATE: name_folder

--- Project12/index.php
+++ Project12/index.php
@@ -20,7 +20,7 @@ $navItems = [
     <title>Menu Navigation</title>
     <!-- Thêm link để sử dụng Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="./assets/css/css.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 <nav>
