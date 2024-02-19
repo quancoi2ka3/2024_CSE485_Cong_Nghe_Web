cdd17c91a7d9eb39113ccb3a01226c970aa18f87
Nguyễn Quân <nguyenmanhquan17072003@gmail.com>
2024-02-19 15:34:35 +0700

    fixed index12

--- Project12/index.php
+++ Project12/index.php
@@ -1,1 +1,16 @@
 <?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC & THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ" ];
+echo '<nav><ul>';
+foreach ($navItems as $item) {
+    echo "<li>$item</li>";
+}
+echo '</ul></nav>'; ?>
