6253fcc3e82c11cb8d1e0e40f9bcd9039352c44f
Nguyễn Quân <nguyenmanhquan17072003@gmail.com>
2024-02-20 20:48:28 +0700

    tiep tuc lam

--- Project12/index.php
+++ Project12/index.php
@@ -1,6 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="/Project12/assets/css/css.css" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
 <?php
-$navItems = [
-    "GIỚI THIỆU",
+    $navItems = [     "GIỚI THIỆU",
     "TIN TỨC & THÔNG BÁO",
     "TUYỂN SINH",
     "ĐÀO TẠO",
@@ -9,8 +20,14 @@ $navItems = [
     "VĂN BẢN",
     "SINH VIÊN",
     "LIÊN HỆ" ];
-echo '<nav><ul>';
-foreach ($navItems as $item) {
-    echo "<li>$item</li>";
-}
-echo '</ul></nav>'; ?>
+    echo '<nav class="navbar" >
+    
+    <ul><i class="fas fa-home home-icon"></i>';
+    foreach ($navItems as $item) {
+        echo "<li class='nav-item' >$item</li>";
+    }
+    echo '</ul></nav>';
+    ?>
+
+</body>
+</html>
--- Project13/index.php
+++ Project13/index.php
@@ -1,1 +1,62 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Danh sách khóa học</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="/Project13/assets/css/css.css" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar"><h2 id="head">KHÓA HỌC SẮP KHAI GIẢNG</h2></nav>
+<div class="container-fluid">
+    <?php
+    $courses = [['title' => 'Học viên quốc tế',
+        'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên quốc tế.',
+        'fee' => '15.000.000 VND',
+        'start_date' => '2/2/24',
+        'duration' => '2 - 2.5 năm'],
+        ['title' => 'Học viên quốc tế',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên quốc tế.',
+            'fee' => '15.000.000 VND',
+            'start_date' => '2/2/24',
+            'duration' => '2 - 2.5 năm'],
+        ['title' => 'Học viên quốc tế',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên quốc tế.',
+            'fee' => '15.000.000 VND',
+            'start_date' => '2/2/24',
+            'duration' => '2 - 2.5 năm'],
+        ['title' => 'Học viên quốc tế',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên quốc tế.',
+            'fee' => '15.000.000 VND',
+            'start_date' => '2/2/24',
+            'duration' => '2 - 2.5 năm'],
+        ['title' => 'Học viên quốc tế',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên quốc tế.',
+            'fee' => '15.000.000 VND',
+            'start_date' => '2/2/24',
+            'duration' => '2 - 2.5 năm'],
+        ['title' => 'Học viên quốc tế',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Global. Phát triển nghề nghiệp sinh viên IT như một lập trình viên quốc tế.',
+            'fee' => '15.000.000 VND',
+            'start_date' => '2/2/24',
+            'duration' => '2 - 2.5 năm']
+    ];
+    $images =[];
+    foreach ($courses as $course) {
+        echo '<div class="course">';
+        echo'<img src="https://aptech.vn/lap-trinh-vien-quoc-te-adse">';
+        echo '<h2>' . $course['title'] . '</h2>';
+        echo '<p>' . $course['description'] . '</p>';
+        echo '<p>Học phí: ' . $course['fee'] . '</p>';
+        echo '<p>Khải giảng: ' . $course['start_date'] . '</p>';
+        echo '<p>Thời lượng: ' . $course['duration'] . '</p>';
+        echo '</div>'; }
+    ?>
+
+</div>
+</body>
+</html>
