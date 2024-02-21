929d5721a7d92e0315deb8cceec962a1a942dfd8
Nguyễn Quân <nguyenmanhquan17072003@gmail.com>
2024-02-21 09:38:31 +0700

    tiep tuc fixed

--- Project13/index.php
+++ Project13/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="/Project13/assets/css/css.css" >
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/41.1.0/classic/ckeditor.js"></script>
 </head>
 <body>
 <nav class="navbar"><h2 id="head">KHÓA HỌC SẮP KHAI GIẢNG</h2></nav>
--- Project14/index.php
+++ Project14/index.php
@@ -1,1 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="/Project14/assets/css/css.css" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<?php
+
+//    foreach ($courses as $course) {
+//        echo '<div class="course">';
+//        echo'<img src="https://aptech.vn/lap-trinh-vien-quoc-te-adse">';
+//        echo '<h2>' . $course['title'] . '</h2>';
+//        echo '<p>' . $course['description'] . '</p>';
+//        echo '<p>Học phí: ' . $course['fee'] . '</p>';
+//        echo '<p>Khải giảng: ' . $course['start_date'] . '</p>';
+//        echo '<p>Thời lượng: ' . $course['duration'] . '</p>';
+//        echo '</div>'; }
+//    ?>
+</body>
+</html>
+
 <?php
