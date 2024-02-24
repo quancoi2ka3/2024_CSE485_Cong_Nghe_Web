52970fc2770be6aa3f6b0bae1fc79c6df804ef02
Nguyễn Quân <nguyenmanhquan17072003@gmail.com>
2024-02-24 07:06:24 +0700

    sua ten file

--- Project14/index.php
+++ Project14/index.php
@@ -2,33 +2,32 @@
 <html lang="en">
 
 <head>
-   <title>Bootstrap Example</title>
+   <title>Bài tập thực hành 14</title>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
    <link rel="stylesheet" href="/Project14/assets/css/css.css">
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.ckeditor.com/ckeditor5/41.1.0/classic/ckeditor.js"></script>
 </head>
 
 <body>
-   <div>
-      <h1>Anh</h1>
+   <div class="form">
+      <div class="one">
+         <div class="nav1">
+            <h3>Basic Info</h3>
+         </div>
+         <div class="main1"></div>
+      </div>
+   </div>
+   <div class="two"></div>
+   <div class="three"></div>
+
    </div>
    <?php
 
-//    foreach ($courses as $course) {
-//        echo '<div class="course">';
-//        echo'<img src="https://aptech.vn/lap-trinh-vien-quoc-te-adse">';
-//        echo '<h2>' . $course['title'] . '</h2>';
-//        echo '<p>' . $course['description'] . '</p>';
-//        echo '<p>Học phí: ' . $course['fee'] . '</p>';
-//        echo '<p>Khải giảng: ' . $course['start_date'] . '</p>';
-//        echo '<p>Thời lượng: ' . $course['duration'] . '</p>';
-//        echo '</div>'; }
-//    ?>
+    ?>
 </body>
 
 </html>
-
-<?php
