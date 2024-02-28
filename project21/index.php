c6f9fa0db0b812fd01441a5c4fdfe3b299ef803f
Quân Nguyễn <nguyenmanhquan17072003@gmail.com>
2024-02-28 19:33:36 +0700

    fix

--- project21/index.php
+++ project21/index.php
@@ -45,8 +45,19 @@
             "price" => 19.99, "description" => "A sexy"
          ],
       ];
-      foreach ($products as $product) {
-         echo '<div class="product" style ="">';
+
+      ?>
+
+   </div>
+   <div class="list-item" style="display:flex;justify-content:space-around">
+
+      <?php
+      $itemsPerPage = 3;
+      $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+      $currentPageItems = array_slice($products, ($currentPage - 1) * $itemsPerPage, $itemsPerPage);
+      $totalPages = ceil(count($products) / $itemsPerPage);
+      foreach ($currentPageItems as $product) {
+         echo '<div class="product" >';
          echo '<div class="image" style ="">';
          echo '<img src="' . $product['image_url'] . '" alt="' . $product['id'] . '">';
          echo '</div>';
@@ -57,14 +68,14 @@
       ?>
    </div>
    <div class="pagination"> <?php
-                              $itemsPerPage = 4;
-                              $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
-                              $currentPageItems = array_slice($products, ($currentPage - 1) * $itemsPerPage, $itemsPerPage);
                               if ($currentPage > 1) : ?> <a href="?page=<?php echo $currentPage - 1; ?>">Previous</a>
-      <?php endif; ?> <?php for ($i = 1; $i <= 3; $i++) : ?> <?php if ($i == $currentPage) : ?> <span
-         class="active"><?php echo $i; ?></span> <?php else : ?> <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-      <?php endif; ?> <?php endfor; ?> <?php if ($currentPage < 3) : ?> <a
-         href="?page=<?php echo $currentPage + 1; ?>">Next</a> <?php endif; ?> </div>
+      <?php endif; ?> <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+         <?php if ($i == $currentPage) : ?>
+            <span class="active"><?php echo $i; ?></span> <?php else : ?> <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+         <?php endif; ?> <?php endfor; ?> <?php if ($currentPage < $totalPages) : ?> <a href="?page=<?php echo $currentPage + 1; ?>">Next</a> <?php endif; ?>
+   </div>
+
+
 
 </body>
 
