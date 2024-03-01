280f19b1df74607880c5641fc74d865a4f358f36
huyl43336 <huyl43336@gmail.com>
2024-03-01 20:18:30 +0700

    .

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+                  
+                  // $color=['c1'=>'red','c2'=>'green'];
+                  // echo "<h1 style='color:{$color['c1']}'>h1</h1>";
+                  // foreach($color as $key => $value){
+                  //   echo $value;
+                  // }
+
+                   
+</body>
+</html> -->
+<?php
+include 'Person.php';
+$person1= new Person('h2123','ádsd');
+?>
